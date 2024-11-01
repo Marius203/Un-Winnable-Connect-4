2b6774c656ab60a91005ffbfa559b9aabe703512
Marius203 <146844907+Marius203@users.noreply.github.com>
2024-11-01 17:11:48 +0200

    final final project

--- player.py
+++
@@ -1,7 +0,0 @@
-import random
-
-class Player:
-    def __init__(self, is_computer) -> None:
-        self.is_computer = is_computer
-        self.name = "x" if self.is_computer == 0 else "o"
-        self.color = "green" if self.is_computer == 0 else "red"
